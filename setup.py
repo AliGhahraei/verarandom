536c55f49c9c3759ffb17d9ce5f8415baee3566c
Ali Ghahraei <aligf94@gmail.com>
2018-09-06 02:27:56 -0500

    Add initial requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='verarandom',
+    version='0.0.1',
+    description='True random numbers (provided by random.org) in Python',
+    author='Ali Ghahraei Figueroa',
+    author_email='aligf94@gmail.com',
+    url='https://github.com/AliGhahraei/verarandom',
+    license='MIT',
+
+    packages=find_packages(),
+)

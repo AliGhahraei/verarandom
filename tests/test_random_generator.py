914a1cd6a6baa0ec01b8b576f69d491bbffdd70a
Ali Ghahraei <aligf94@gmail.com>
2018-09-06 21:08:02 -0500

    Rename fixture and add quota check to random number tests

--- tests/test_random_generator.py
+++ tests/test_random_generator.py
@@ -1,4 +1,5 @@
 from typing import Callable
+from unittest import mock
 
 import responses
 from assertpy import assert_that
@@ -14,61 +15,79 @@ VeraFactory = Callable[..., VeraRandom]
 
 
 @fixture
-def mock_vera() -> VeraFactory:
-    def _mock_vera(quota: int = MAX_QUOTA):
-        _mock_response(QUOTA_URL, body=str(quota))
+def patch_vera() -> VeraFactory:
+    def _patch_vera(quota: int = MAX_QUOTA):
+        _patch_response(QUOTA_URL, body=str(quota))
         return VeraRandom()
 
-    return _mock_vera
+    return _patch_vera
+
+
+def _patch_int_response(body: str, **kwargs):
+    _patch_response(INTEGER_URL, body=body, **kwargs)
+
+
+def _patch_response(url: str, method: str=responses.GET, **kwargs):
+    responses.add(method, url, **kwargs)
 
 
 @responses.activate
-def test_valid_quota(mock_vera: VeraFactory):
-    mock_vera().check_quota()
+def test_valid_quota(patch_vera: VeraFactory):
+    patch_vera().check_quota()
 
 
 @responses.activate
-def test_valid_quota_at_limit(mock_vera: VeraFactory):
-    mock_vera(QUOTA_LIMIT).check_quota()
+def test_valid_quota_at_limit(patch_vera: VeraFactory):
+    patch_vera(QUOTA_LIMIT).check_quota()
 
 
 @responses.activate
-def test_invalid_quota_below_limit(mock_vera: VeraFactory):
-    mock = mock_vera(QUOTA_LIMIT - 1)
-    assert_that(mock.check_quota).raises(RandomOrgQuotaExceeded)
+def test_invalid_quota_below_limit(patch_vera: VeraFactory):
+    vera = patch_vera(QUOTA_LIMIT - 1)
+    assert_that(vera.check_quota).raises(RandomOrgQuotaExceeded)
 
 
 @responses.activate
-def test_invalid_quota_response(mock_vera: VeraFactory):
-    _mock_response(QUOTA_URL, status=500)
-    assert_that(mock_vera).raises(HTTPError)
+def test_invalid_quota_response(patch_vera: VeraFactory):
+    _patch_response(QUOTA_URL, status=500)
+    assert_that(patch_vera).raises(HTTPError)
 
 
+@mark.parametrize('response', [('17',)])
 @mark.xfail
 @responses.activate
-def test_random(mock_vera: VeraFactory):
-    assert_that(mock_vera().random()).is_greater_than_or_equal_to(0).is_less_than(1)
+def test_random(patch_vera: VeraFactory, response: str):
+    vera = patch_vera()
+    _patch_int_response(response)
+
+    with mock.patch.object(vera, 'check_quota') as check_quota:
+        assert_that(patch_vera().random()).is_greater_than_or_equal_to(0).\
+            is_less_than(1)
+
+        check_quota.assert_called_once()
 
 
 @mark.parametrize('lower, upper, response', [(1, 20, '17')])
 @responses.activate
-def test_randint(mock_vera: VeraFactory, lower: int, upper: int, response: str):
-    _mock_int_response(response)
-    assert_that(mock_vera().randint(lower, upper)).is_greater_than_or_equal_to(lower).\
-        is_less_than_or_equal_to(upper)
+def test_randint(patch_vera: VeraFactory, lower: int, upper: int, response: str):
+    vera = patch_vera()
+    _patch_int_response(response)
+
+    with mock.patch.object(vera, 'check_quota') as check_quota:
+        assert_that(vera.randint(lower, upper)).is_greater_than_or_equal_to(lower).\
+            is_less_than_or_equal_to(upper)
+
+        check_quota.assert_called_once()
 
 
 @mark.parametrize('lower, upper, n, response', [(1, 3, 5, '3\n3\n1\n2\n1')])
 @responses.activate
-def test_randints(mock_vera: VeraFactory, lower: int, upper: int, n: int, response: str):
-    _mock_int_response(response)
-    for random in mock_vera().randints(lower, upper, n):
-        assert_that(random).is_greater_than_or_equal_to(lower).is_less_than_or_equal_to(upper)
+def test_randints(patch_vera: VeraFactory, lower: int, upper: int, n: int, response: str):
+    vera = patch_vera()
+    _patch_int_response(response)
 
+    with mock.patch.object(vera, 'check_quota') as check_quota:
+        for random in vera.randints(lower, upper, n):
+            assert_that(random).is_greater_than_or_equal_to(lower).is_less_than_or_equal_to(upper)
 
-def _mock_response(url: str, method: str=responses.GET, **kwargs):
-    responses.add(method, url, **kwargs)
-
-
-def _mock_int_response(body: str, **kwargs):
-    _mock_response(INTEGER_URL, body=body, **kwargs)
+        check_quota.assert_called_once()

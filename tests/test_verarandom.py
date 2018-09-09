7bd4ae32146f8b20a14804529ccbeb36396ed322
Ali Ghahraei <aligf94@gmail.com>
2018-09-08 19:29:01 -0500

    Make verarandom a module instead of a package

--- tests/test_verarandom.py
+++ tests/test_verarandom.py
@@ -6,7 +6,7 @@ from assertpy import assert_that
 from pytest import fixture, mark
 from requests import HTTPError
 
-from verarandom.random_generator import (
+from verarandom import (
     VeraRandom, QUOTA_URL, QUOTA_LIMIT, MAX_QUOTA, BitQuotaExceeded, INTEGER_URL,
     MAX_NUMBER_OF_INTEGERS, TooManyRandomNumbersRequested, MAX_INTEGER_LIMIT,
     RandomNumberLimitTooLarge, NoRandomNumbersRequested, MIN_INTEGER_LIMIT,

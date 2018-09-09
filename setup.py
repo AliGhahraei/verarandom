3611534ee490339a80bcfd0280c1e23fc23414c8
Ali Ghahraei <aligf94@gmail.com>
2018-09-09 16:19:28 -0500

    Prepare setup.py for PyPI upload

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name='verarandom',
     version='0.0.1',
@@ -9,5 +12,14 @@ setup(
     url='https://github.com/AliGhahraei/verarandom',
     license='MIT',
 
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
     packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
 )
